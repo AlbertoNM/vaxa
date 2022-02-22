a011ae2bb02a49954d691cf1ec4d472fe01d8b6f
Alberto <alberto-city@hotmail.com>
2022-02-22 01:50:20 -0600

    First commit - Ordering the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { MainText } from './components/home/MainText/MainText'
+
+function App() {
+  return (
+    <div className="App">
+			<MainText/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/MainText/MainText.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './MainText.css';
+
+function MainText() {
+	return(
+		<section className="MainText">
+			<h3 className="MainText__title">Lorem Ipsum</h3>
+			<h3 className="MainText__subtitle">Lorem ipsum dolor sit amet consectetur</h3>
+			<h3 className="MainText__paragraph">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, ut adipisci. Harum earum iste optio repudiandae quidem omnis aspernatur vel eaque, id praesentium, commodi recusandae nam non dolore officia porro.</h3>
+			<h3 className="MainText__text">Lorem, ipsum dolor</h3>
+		</section>
+	);
+}
+
+export { MainText };
